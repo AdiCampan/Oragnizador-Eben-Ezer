9fada06cda21e98af7a70ac87ec613ebab07dd91
Adi Campan <adicampan1974@gmail.com>
2024-03-03 17:05:25 +0100

    add gradient background

--- App.js
+++ App.js
@@ -1,19 +1,24 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Add User</Text>
+    <LinearGradient
+      style={styles.background}
+      colors={["#4c669f", "#3b5998", "#192f6a"]}
+      start={{ x: 0, y: 0 }}
+      end={{ x: 1, y: 1 }}
+    >
+      <Text>EBEN EZER </Text>
       <StatusBar style="auto" />
-    </View>
+    </LinearGradient>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  background: {
     flex: 1,
-    backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
   },
