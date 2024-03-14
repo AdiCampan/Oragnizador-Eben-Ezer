2686eb1c87f59a724c46a124514f7824f1207d3d
Adi Campan <adicampan1974@gmail.com>
2024-03-14 20:41:01 +0100

    add datePicker, firebase Adi

--- App.js
+++ App.js
@@ -1,54 +1,8 @@
-import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
-import { LinearGradient } from "expo-linear-gradient";
-import {NavigationContainer} from '@react-navigation/native';
+import Navigation from "./Navigation";
 
-export default function App({navigation}) {
-  const addPerson = () =>{}
-  return (
-    <NavigationContainer>
-      <LinearGradient
-      style={styles.background}
-      colors={["#4c669f", "#3b5998", "#192f6a"]}
-      start={{ x: 0, y: 0 }}
-      end={{ x: 1, y: 1 }}
-     >
-       <StatusBar style="auto" />
-      <Text>EBEN EZER </Text>
-      <TouchableOpacity onPress={() =>
-        navigation.navigate('Cantari')}>
-      <LinearGradient
-      colors={["#004d40", "#009688"]}
-      style={styles.appButtonContainer}
-      >
-      <Text style={styles.appButtonText}>Adauga un utilizator
-    
-      </Text>
-      </LinearGradient>
-         </TouchableOpacity>
-     
-        </LinearGradient>
-    </NavigationContainer>
-  );
+export default function App() {
+  return <Navigation />;
 }
 
-const styles = StyleSheet.create({
-  background: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  appButtonContainer: {
-    elevation: 8,
-    borderRadius: 10,
-    paddingVertical: 10,
-    paddingHorizontal: 12
-  },
-  appButtonText: {
-    fontSize: 18,
-    color: "#fff",
-    fontWeight: "bold",
-    alignSelf: "center",
-    textTransform: "uppercase"
-  }
-});
+const styles = StyleSheet.create({});
--- firebase.js
+++ firebase.js
@@ -1,21 +1,30 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { getDatabase } from "firebase/database";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBjJLkYgPeoTffA1hjYvE_Ny9Gp2W41k_U",
-  authDomain: "ebentizer.firebaseapp.com",
-  projectId: "ebentizer",
-  storageBucket: "ebentizer.appspot.com",
-  messagingSenderId: "826182261195",
-  appId: "1:826182261195:web:cb7f4e5c48aadd13c5be4e"
+  apiKey: "AIzaSyDCLvO6kslY5NYdS2JzWeh5AqVWHSoAcd0",
+  authDomain: "organizer-ee1d1.firebaseapp.com",
+  projectId: "organizer-ee1d1",
+  storageBucket: "organizer-ee1d1.appspot.com",
+  messagingSenderId: "439942894629",
+  appId: "1:439942894629:web:b888ddc1ce5bf24d1adbcc",
+
+  databaseURL:
+    "https://organizer-ee1d1-default-rtdb.europe-west1.firebasedatabase.app/",
+  // database:
+  //   "https://organizer-ee1d1-default-rtdb.europe-west1.firebasedatabase.app",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 // Initialize Cloud Firestore and get a reference to the service
 const db = getFirestore(app);
-export{db};
+// Initialix=ze RealTimeDatBAse
+const database = getDatabase(app);
+export { db, database };
